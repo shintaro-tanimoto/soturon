9b0260c627ed495f17600252c8e146843f8dc60b
shintaro-tanimoto <shintarotanigen@gmail.com>
2025-07-02 16:15:56 +0900

    test

--- make_spacefillingunit.py
+++ make_spacefillingunit.py
@@ -1,4 +1,12 @@
-import find_graph
+import find_graph as fg
+import find_coordinates as fc
+import networkx as nx
 
-unit_number = 13
-print(find_graph.find_filtered_edges_list(unit_number))
+unit_number = 8
+unique_graphs_list = fg.find_unique_graphs_list(unit_number)
+
+"""fg.draw_graph(unique_graphs_list)"""
+
+#test
+print(unique_graphs_list[0].nodes[0])
+
